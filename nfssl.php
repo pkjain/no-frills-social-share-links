5c7958a3238c9e9f953b0a74665ec97a88968e53
Parveen Kumar <pkjain@gmail.com>
2013-03-17 11:58:13 -0400

    nfssl.php file containing plugin code

---
+++ nfssl.php
@@ -0,0 +1,74 @@
+<?php
+/*
+Plugin Name: No frills social share links (NFSSL)
+Plugin URI: http://infoheap.com/
+Description: No Frill Social Share buttons for twitter and facebook
+Author: infoheap team
+Author URI: http://infoheap.com/
+Version: 1.0
+*/
+
+
+function nfssl_the_content_filter ($content) {
+  $posturl = urlencode(get_permalink());
+  $posttitle = urlencode(html_entity_decode(get_the_title(), ENT_COMPAT, 'UTF-8'));
+  $share_html = "<div class='nfssl'>Share: ";
+
+  // G+
+  $link = "https://plus.google.com/share?url=$posturl";
+  $title = "share on Google+";
+  $img = "/img/Google_plus_icon-18.png";
+  $share_html .= " <a title='$title' href='$link' target='_blank'><img src='$img' alt='$title' /></a>";
+
+  // fb
+  $link = "http://www.facebook.com/share.php?u=$posturl";
+  $title = "share on facebook";
+  $img = "/img/F_icon-18.png";
+  $share_html .= " <a title='$title' href='$link' target='_blank'><img src='$img' alt='$title' /></a>";
+
+  // linkedin
+  $link = "http://www.linkedin.com/shareArticle?mini=true&url=$posturl" . "&title=$posttitle";
+  $title = "share on linkedin";
+  $img = "/img/Linkedin_icon-18.png";
+  $share_html .= " <a title='$title' href='$link' target='_blank'><img src='$img' alt='$title' /></a>";
+
+  // twitter
+  $link = "https://twitter.com/intent/tweet?url=$posturl" .  "&text=$posttitle";
+  $title = "tweet this";
+  $img = "/img/Twitter-18.png";
+  $share_html .= " <a title='$title' href='$link' target='_blank'><img src='$img' alt='$title' /></a>";
+
+  // reddit
+  $link = "http://www.reddit.com/submit?url=$posturl" . "&title=$posttitle";
+  $title = "submit to reddit";
+  $img = "/img/Reddit-18.png";
+  $share_html .= " <a title='$title' href='$link' target='_blank'><img src='$img' alt='$title' /></a>";
+
+  // delicious
+  /*
+  $link = "http://del.icio.us/post?url=$posturl" . "&amp;title=$posttitle";
+  $title = "share on delicious";
+  $img = "/img/Delicious-18.png";
+  $share_html .= " <a title='$title' href='$link'><img src='$img' alt='$title' /></a>";
+  */
+
+  // stumbleupon
+  $link = "http://www.stumbleupon.com/submit?url=$posturl";
+  $title = "share on stumbleupon";
+  $img = "/img/Stumbleupon-18.png";
+  $share_html .= " <a title='$title' href='$link' target='_blank'><img src='$img' alt='$title' /></a>";
+
+  //digg
+  $link = "http://digg.com/submit?url=$posturl" . "&title=$posttitle";
+  $title = "share on digg";
+  $img = "/img/Digg_Shiny_Icon-18.png";
+  $share_html .= " <a title='$title' href='$link'><img src='$img' alt='$title' target='_blank' /></a>";
+
+  $share_html .= "</div>\n";
+  $content .= $share_html;
+  return $content;
+}
+add_filter( 'the_content', 'nfssl_the_content_filter');
+
+
+?>
